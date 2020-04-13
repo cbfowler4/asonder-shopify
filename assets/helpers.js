d1a2c036d86f0f38a7af7915e20572a316e84cb8
Bryan <cbfowler4@gmail.com>
2020-04-13 12:12:29 -0400

    updates COVID warning

--- assets/helpers.js
+++ assets/helpers.js
@@ -1,6 +1,14 @@
 const { moment } = window;
 
-const updateShipTimeElement = (element) => {
+const updateEstimatedShippingElement = () => {
+  const element = document.getElementById('shipping-object');
+
+  if (!element) {
+    const estimatedShippingEl = document.getElementById('estimated-shipping');
+    estimatedShippingEl.setAttribute('style', 'display: none');
+    return;
+  }
+
   const FORMAT = 'MMM D'
   const time = element.innerText.split(',');
   const newInnerText = `${moment().add(time[0], 'd').format(FORMAT)} and ${moment().add(time[1], 'd').format(FORMAT)}`
